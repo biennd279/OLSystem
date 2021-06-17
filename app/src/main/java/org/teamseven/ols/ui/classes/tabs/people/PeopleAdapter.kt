27fef405ac9684944cd1dbc564ceebf63e5defc6
Green <nguyetnguyen15802@gmail.com>
2021-06-18 03:46:57 +0700

    Revert "old version, remove it"
    This reverts commit 8d83a9b69dbc09298683b0d41dbc9b71c9e2d05d.

--- app/src/main/java/org/teamseven/ols/ui/classes/tabs/people/PeopleAdapter.kt
+++ app/src/main/java/org/teamseven/ols/ui/classes/tabs/people/PeopleAdapter.kt
@@ -2,50 +2,84 @@ package org.teamseven.ols.ui.classes.tabs.people
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import org.teamseven.ols.R
+import org.teamseven.ols.databinding.ItemMemberViewBinding
+import org.teamseven.ols.entities.User
 
 class PeopleAdapter(
     private val context: Context,
-    private val peopleItems: List<PeopleItem>,
-    private val listener: (PeopleItem) -> Unit
-) : RecyclerView.Adapter<PeopleAdapter.ViewHolder>() {
+    private var peopleItems: List<User>,
+    private val listener: (User) -> Unit
+) : ListAdapter<User, RecyclerView.ViewHolder>(PeopleCallBack()) {
 
 
     //ViewHolder
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val username = view.findViewById<TextView>(R.id.text_member_name)
-        val avatar = view.findViewById<ImageView>(R.id.img_member_avatar)
+    class ViewHolder private constructor(
+        private val binding: ItemMemberViewBinding
+        ): RecyclerView.ViewHolder(binding.root) {
 
-        fun bindItem(items: PeopleItem, listener: (PeopleItem) -> Unit) {
-            username.text = items.username
+        private val username = binding.textMemberName
+        private val avatar = binding.imgMemberAvatar
 
-            Glide.with(itemView.context).load(items.avatar).into(avatar)
+        fun bind(item: User, listener: (User) -> Unit) {
+            username.text = item.name
+
+            if (item.avatarUrl != null) {
+                Glide.with(itemView.context).load(item.avatarUrl).into(avatar)
+            } else {
+                Glide.with(itemView.context).load(R.drawable.ic_person_outline).into(avatar)
+            }
+
             itemView.setOnClickListener{
-                listener(items)
+                listener(item)
+            }
+
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val binding = ItemMemberViewBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ViewHolder(binding)
             }
         }
     }
 
-
     // Create new views (invoked by the layout manager
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_member_view, parent, false)
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
 
     // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.bindItem(peopleItems[position], listener)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ViewHolder -> {
+                holder.bind(peopleItems[position], listener)
+            }
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = peopleItems.size
+}
+
+class PeopleCallBack(): DiffUtil.ItemCallback<User>() {
+    override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+        return oldItem == newItem
+    }
 
 }
